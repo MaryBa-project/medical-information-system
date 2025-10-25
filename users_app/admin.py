66eb3e5b028368ccc6e3c97a476d647835461d91
MaryBa-project <mariba1304.work@gmail.com>
2025-10-25 15:29:56 +0300

    add fixtures for group_user and test_users, fix class CustomUserAdmin

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Doctor, Personnel, Patient, LaboratoryAssistent, CustomUser, FamilyDoctor
 
+class CustomUserAdmin(UserAdmin):
+    # Додаємо до стандартних полів нові в форму додавання користувача
+  add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Персональна інформація', {'fields': ('email','last_name', 'first_name', 
+                                                'patronymic', 'groups', 'phone_number', 'img')}),
+    )
 
-admin.site.register(CustomUser)
+    # Тут ми додаємо ТІЛЬКИ нові поля для кастом юзер, щоб уникнути дублювання email, first_name, last_name..
+  fieldsets = UserAdmin.fieldsets + (
+        ('Додаткова інформація профілю', {'fields': ('patronymic', 'phone_number', 'img')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Doctor)
 admin.site.register(Personnel)
 admin.site.register(Patient)
